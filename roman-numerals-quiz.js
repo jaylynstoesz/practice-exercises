d94395cd8a226e0ba91dc1147e74222ec7d2d0aa
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-09-09 10:41:58 -0600

    text formatting

--- roman-numerals-quiz.js
+++ roman-numerals-quiz.js
@@ -1,8 +1,17 @@
 // Convert Roman numeral inputs to numbers using a recursive function
 
-// Here is a recursive function you could use to solve this problem. First, convert the Roman numerals into the values they represent. Then, run logic to find out if you need to subtract any of the values from another to get the correct answer: until you get to much larger numbers, the only times you need to subtract as opposed to simply totaling the numbers is when you have a 4 or a 9, in which cases the last numeral is not equal to 1 AND the second to last numeral is equal to 1. So, if these two conditions exist, simply replace the last two numerals with their difference (either 10 - 1 = 9 or 5 - 1 = 4) and find the sum of the remaining numerals using an accumulator variable and an index counter.
+// Here is a recursive function you could use to solve this problem.
+// First, convert the Roman numerals into the values they represent.
+// Then, run logic to find out if you need to subtract any of the values from another to get the correct answer:
+// until you get to much larger numbers, the only times you need to subtract as opposed to simply totaling the
+// numbers is when you have a 4 or a 9, in which cases the last numeral is not equal to 1 AND the second to last
+// numeral is equal to 1. So, if these two conditions exist, simply replace the last two numerals with their
+// difference (either 10 - 1 = 9 or 5 - 1 = 4) and find the sum of the remaining numerals using an accumulator
+// variable and an index counter.
 
-// However, I would not use a recursive function to solve this problem. I would use the .reduce method in roman-numerals.js, as it's basically the same functionality built into JavaScript. In the other solution you do not need global variables to get the job done, while here you need three.
+// However, I would not use a recursive function to solve this problem. I would use the .reduce method in
+// roman-numerals.js, as it's basically the same functionality built into JavaScript. In the other solution you do
+// not need global variables to get the job done, while here you need three.
 
 var input = ('III')
 var input2 = ('IV')
