b87374aba6014c380939af40a7348ff1e2ebd50e
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-09-16 15:04:44 -0600

    fuzzy match solution

---
+++ fuzzy-match.js
@@ -0,0 +1,24 @@
+var inputs = ["James", "Samantha", "Mark", "Sarah", "Manuel"]
+
+function fuzzyMatch (inputs, searchTerm, tolerance) {
+
+  return inputs.filter(function (input) {
+    var splitInput = input.toLowerCase().split("")
+    var splitSearchTerm = searchTerm.toLowerCase().split("")
+    var matches = []
+    for (var i = 0; i < splitInput.length; i++) {
+      counter = 0
+      for (var j = 0; j < splitSearchTerm.length; j++) {
+        if (splitInput[i + j] === splitSearchTerm[j]) {
+          counter++
+        }
+      }
+      if (counter >= searchTerm.length - tolerance) {
+        return input
+      }
+    }
+  })
+
+}
+
+console.log(fuzzyMatch(inputs, "man", 1))
